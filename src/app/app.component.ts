ad544425630b11ac2ae20b58e5ad6fd429b5b60d
prangya <kar.prangya123@gmail.com>
2021-10-03 16:57:14 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { startWith } from 'rxjs/operators';
+import { JsonService } from './json.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'yebelotechnologyAssignment';
+  Submission! :FormGroup;
+  submitted=false;
+  public jsonData: any;
+  public jsonDataGlobal: any;
+  selected=true;
+filtered :any;
+categoriesList:any;
+myControl = new FormControl();
+  constructor(private formBuilder: FormBuilder,
+    private jsonService: JsonService) {
+  }
+  
+  public ngOnInit(): void {
+    this.jsonService.getData()
+      .subscribe((data: any): void => {
+        console.info(data);
+        this.jsonData = data;
+        this.jsonDataGlobal=data;
+        this.selectCategories();
+      });
+      this.myControl.valueChanges.subscribe(val=>{
+       
+        this. onOptionsSelected(val);
+      })
+      
+      ;
+   
+  }
+  onOptionsSelected(val:any) {
+debugger;
+    this.jsonData=val=="All"?this.jsonDataGlobal:this.jsonDataGlobal.filter((t:any)=>t.p_category==val)
+
+  }
+  selectCategories(){
+    this.categoriesList=[];
+    this.jsonDataGlobal.forEach((element:any) => {
+      debugger;
+      if(element.p_category){
+        this.categoriesList.push(element.p_category)
+      }
+    });
+  }
+  AddQuantity(){
+    this.jsonDataGlobal.forEach((e:any) => {
+      debugger;
+     e.quantity ="";
+    });
+  }
+  savedata(){
+    // console.log(this.jsonData);
+    console.log(this.jsonDataGlobal)
+    window.alert(JSON.stringify(this.jsonDataGlobal));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { JsonService } from './json.service';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    FormsModule, 
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [JsonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
